f3801a78d83c76d2a081fbeb9671241d3c5b175b
RajibTheKing <aporba.das@gmail.com>
2024-06-28 20:40:57 +0200

    solve unique path using dp

---
+++ Practice_Problems/uniquePaths_DP.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int dp[101][101];
+    int uniquePaths(int m, int n) {
+        for(int i=0; i<m; i++)
+            for(int j=0; j<n; j++)
+                dp[i][j] = -1;
+
+        return solve(0,0,m, n);
+    }
+    int solve(int row, int col, int m, int n)
+    {
+
+        if(row == m - 1 && col == n - 1)
+        {
+            return 1;
+        }
+
+        if(row >= m || col >= n)
+        {
+            return 0;
+        }
+
+        int& ret = dp[row][col];
+        if(ret != -1)
+        {
+            return ret;
+        }
+
+        ret = 0;
+        ret += solve(row+1, col, m, n);
+        ret += solve(row, col+1, m, n);
+
+        return ret;
+    }
+};
