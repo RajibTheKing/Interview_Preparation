9a25d897f5806bf8fddac1541889db3e876f97d1
RajibTheKing <aporba.das@gmail.com>
2024-07-06 13:35:40 +0200

    solve longest valid parantheses, T = O(n), S = O(n)

---
+++ Practice_Problems/Hard/longestValidParantheses.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        
+        return maxValidParantheses(0, s);
+    }
+
+    int maxValidParantheses(int startingPos, string& s)
+    {
+        stack<int> st;
+        st.push(-1);
+        int maxCnt = 0;
+
+        for (int i = startingPos; i < s.size(); i++)
+        {
+            if (s[i] == '(')
+            {
+                st.push(i);
+            }
+            else
+            {
+                st.pop();
+                if (st.empty())
+                    st.push(i);
+
+                maxCnt = max(maxCnt, i - st.top());
+            }
+        }
+        return maxCnt;
+    }
+};
